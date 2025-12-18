88cb621c9228d49099ddbd00d7381e037ebc3979
szeunkk <szeunkk@gmail.com>
2025-12-19 01:42:19 +0900

    style: Layout 컴포넌트 코드 포맷팅 정리

--- src/commons/layout/index.tsx
+++ src/commons/layout/index.tsx
@@ -8,18 +8,11 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className={styles.container}>
-      <aside className={styles.sidebar}>
-        {/* Sidebar content */}
-      </aside>
+      <aside className={styles.sidebar}>{/* Sidebar content */}</aside>
       <div className={styles.main}>
-        <header className={styles.header}>
-          {/* Header content */}
-        </header>
-        <main className={styles.children}>
-          {children}
-        </main>
+        <header className={styles.header}>{/* Header content */}</header>
+        <main className={styles.children}>{children}</main>
       </div>
     </div>
   );
 };
-
