36d7176f8248f87d0800056177e25ba48c0e9b78
Jiyoon-Yang <jiyoon3522@naver.com>
2025-12-20 22:54:30 +0900

    feat: 파티 상세 페이지 타이틀 UI 구현

---
+++ src/components/party-detail/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import Icon from '@/commons/components/icon';
+import { URL_PATHS } from '@/commons/constants/url';
+import styles from './styles.module.css';
+
+export default function PartyDetail() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleArea}>
+        <Link href={URL_PATHS.PARTY} className={styles.backLink}>
+          <Icon name="arrow-left" size={24} className={styles.backIcon} />
+          <span className={styles.backText}>돌아가기</span>
+        </Link>
+        <h1 className={styles.title}>RPG 길드 매칭</h1>
+        <p className={styles.subtitle}>
+          대형 RPG 게임을 함께 즐기는 길드원 모집
+        </p>
+      </div>
+      <div className={styles.mainArea}>
+        <div className={styles.chatArea}>chat-area</div>
+        <div className={styles.sideArea}>
+          <div className={styles.memberList}>member-list</div>
+          <div className={styles.partyInfo}>party-info</div>
+        </div>
+      </div>
+    </div>
+  );
+}
