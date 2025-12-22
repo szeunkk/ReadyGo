39a3b15a4d07759428f95a794acfa9c643879b48
Jiyoon-Yang <jiyoon3522@naver.com>
2025-12-22 11:38:11 +0900

    feat(party-info): 파티 정보 UI 컴포넌트 구현 및 스타일 조정
    - 파티 정보 컴포넌트 구현 (게임, 시작 시간, 난이도, 컨트롤 수준, 보이스챗)
    - Tag 및 Button 공통 컴포넌트 사용
    - infoIcon과 infoLabel을 wrapper로 묶어 레이아웃 개선
    - border-radius 및 border 스타일 조정
    - infoValue 텍스트 색상 변경 (interactive-secondary)

--- src/components/party-detail/ui/party-info/partyInfo.tsx
+++ src/components/party-detail/ui/party-info/partyInfo.tsx
@@ -1,74 +1,71 @@
 'use client';
 
 import React from 'react';
-import styles from './styles.module.css';
 import Icon from '@/commons/components/icon';
 import Tag from '@/commons/components/tag';
 import Button from '@/commons/components/button';
+import styles from './styles.module.css';
 
 export default function PartyInfo() {
   return (
     <div className={styles.container}>
-      <div className={styles.mainArea}>
-        <h2 className={styles.title}>파티 정보</h2>
-
-        <div className={styles.infoList}>
-          <div className={styles.infoItem}>
-            <div className={styles.iconLabelWrapper}>
-              <Icon name="joystick-alt" size={20} className={styles.icon} />
-              <span className={styles.label}>게임</span>
+      <div className={styles.content}>
+        <div className={styles.infoSection}>
+          <div className={styles.infoInner}>
+            <h2 className={styles.title}>파티 정보</h2>
+            <div className={styles.infoList}>
+              <div className={styles.infoItem}>
+                <div className={styles.iconLabelWrapper}>
+                  <Icon name="gaming" size={20} className={styles.infoIcon} />
+                  <span className={styles.infoLabel}>게임</span>
+                </div>
+                <span className={styles.infoValue}>팰월드</span>
+              </div>
+              <div className={styles.infoItem}>
+                <div className={styles.iconLabelWrapper}>
+                  <Icon name="time" size={20} className={styles.infoIcon} />
+                  <span className={styles.infoLabel}>시작 시간</span>
+                </div>
+                <span className={styles.infoValue}>12/25 오후 6:30</span>
+              </div>
+              <div className={styles.infoItem}>
+                <div className={styles.iconLabelWrapper}>
+                  <Icon name="crown" size={20} className={styles.infoIcon} />
+                  <span className={styles.infoLabel}>난이도</span>
+                </div>
+                <span className={styles.infoValue}>지옥</span>
+              </div>
+              <div className={styles.infoItem}>
+                <div className={styles.iconLabelWrapper}>
+                  <Icon name="gamepad" size={20} className={styles.infoIcon} />
+                  <span className={styles.infoLabel}>컨트롤 수준</span>
+                </div>
+                <span className={styles.infoValue}>빡숙</span>
+              </div>
+              <div className={styles.infoItem}>
+                <div className={styles.iconLabelWrapper}>
+                  <Icon name="mic" size={20} className={styles.infoIcon} />
+                  <span className={styles.infoLabel}>보이스챗</span>
+                </div>
+                <span className={styles.infoValue}>필수 사용</span>
+              </div>
             </div>
-            <span className={styles.value}>팰월드</span>
-          </div>
-
-          <div className={styles.infoItem}>
-            <div className={styles.iconLabelWrapper}>
-              <Icon name="time" size={20} className={styles.icon} />
-              <span className={styles.label}>시작 시간</span>
-            </div>
-            <span className={styles.value}>12/25 오후 6:30</span>
-          </div>
-
-          <div className={styles.infoItem}>
-            <div className={styles.iconLabelWrapper}>
-              <Icon name="crown" size={20} className={styles.icon} />
-              <span className={styles.label}>난이도</span>
-            </div>
-            <span className={styles.value}>지옥</span>
-          </div>
-
-          <div className={styles.infoItem}>
-            <div className={styles.iconLabelWrapper}>
-              <Icon name="gamepad" size={20} className={styles.icon} />
-              <span className={styles.label}>컨트롤 수준</span>
-            </div>
-            <span className={styles.value}>빡숙</span>
-          </div>
-
-          <div className={styles.infoItem}>
-            <div className={styles.iconLabelWrapper}>
-              <Icon name="mic" size={20} className={styles.icon} />
-              <span className={styles.label}>보이스챗</span>
-            </div>
-            <span className={styles.value}>필수 사용</span>
           </div>
         </div>
-
-        <div className={styles.tagGroup}>
+        <div className={styles.tags}>
           <Tag style="rectangle">FPS</Tag>
           <Tag style="rectangle">저녁</Tag>
           <Tag style="rectangle">빡겜</Tag>
         </div>
-
-        <Button
-          variant="primary"
-          size="m"
-          shape="round"
-          className={styles.joinButton}
-        >
-          참여하기
-        </Button>
       </div>
+      <Button
+        variant="primary"
+        size="m"
+        shape="round"
+        className={styles.button}
+      >
+        참여하기
+      </Button>
     </div>
   );
 }
