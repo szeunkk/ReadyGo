57bd40cf63b7378138b59207d55d6d37df8e2470
szeunkk <szeunkk@gmail.com>
2025-12-24 00:36:07 +0900

    fix: matchPage를 원래 Figma 디자인으로 복구
    - 헤더 섹션 복원 (제목 + 부제목)
    - 원래 레이아웃 구조 복구 (container > header > content)
    - styles.module.css에 헤더 배경색 추가
    - MatchList 컴포넌트 연결 복원

--- src/components/match/matchPage.tsx
+++ src/components/match/matchPage.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-import { MatchList } from './ui';
+import React from 'react';
+import styles from './styles.module.css';
+import MatchList from './ui/match-list/matchList';
 
 export default function Match() {
-  // TODO: 매칭 페이지 구현
   return (
-    <div>
-      <h1>매칭</h1>
-      <MatchList matches={[]} />
+    <div className={styles.container}>
+      {/* 헤더 섹션 */}
+      <div className={styles.header}>
+        <h1 className={styles.title}>매칭 찾기</h1>
+        <p className={styles.subtitle}>너랑 딱 맞는 게임 친구를 찾아봐</p>
+      </div>
+
+      {/* 매치 리스트 섹션 */}
+      <div className={styles.content}>
+        <MatchList />
+      </div>
     </div>
   );
 }
