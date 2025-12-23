4ad1745848833ce1ddff0b8f41c87503d5f1ee2f
Jiyoon-Yang <jiyoon3522@naver.com>
2025-12-23 16:17:26 +0900

    feat: 파티 관련 컴포넌트 및 페이지 업데이트

---
+++ src/app/temp/page.tsx
@@ -0,0 +1,11 @@
+import PartySubmit from '@/components/party-submit';
+
+export default function TempPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <PartySubmit />
+    </div>
+  );
+}
+
+
